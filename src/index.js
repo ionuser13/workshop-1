3f6d3f08c30dc4a03f0bf4caa73b48bc94c8ffec
userjohn <johnchacpi479@gmail.com>
2022-11-09 22:45:25 -0500

    connected to api and added basic containers

--- src/index.js
+++ src/index.js
@@ -3,4 +3,21 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const urlAvo = "https://platzi-avo.vercel.app/api/avo";
+
+async function fetchData() {
+    const response = await fetch(urlAvo);
+    const data = await response.json();
+    let items = [];
+    data.data.forEach((item) => {
+        const image = document.createElement("img");
+        const title = document.createElement("h2");
+        const price = document.createElement("div");
+        const container = document.createElement("div");
+        container.append(image, title, price);
+        items.push(container);
+    })
+    document.body.append(...items)
+    console.log(data)
+}
+fetchData()
