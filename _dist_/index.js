7121b1ad4e8d3463190be4d8ad4c919dfee416fc
ionuser13 <ionuser13@users.noreply.github.com>
2022-11-11 17:53:28 +0000

    deploy: 7eddc83d56cdc2849aac763fa272c9997a3ea27c

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,12 +1,7 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
-
 const baseURL = "https://platzi-avo.vercel.app";
 const urlApi = "https://platzi-avo.vercel.app/api/avo";
 const myAppNode = document.querySelector("#app");
-myAppNode.className = "grid grid-cols-3 gap-4 max-w-5xl mx-auto";
+myAppNode.className = "grid grid-cols-4 gap-8 max-w-7xl px-4 py-8 mx-auto min-w-fit rounded-2xl";
 
 // Intl API 1.- dates formats 2.- currency formats
 
@@ -26,16 +21,17 @@ async function fetchData() {
         dataRes.data.forEach((item) => {
             const image = document.createElement("img");
             image.src = `${baseURL}${item.image}`;
-            image.className = "w-full cursor-pointer"
+            image.className = "w-5/6 mx-auto rounded-full"
 
             const title = document.createElement("h2");
             title.textContent = item.name;
-            title.className = "text-xl text-red-600 cursor-pointer"
+            title.className = "text-xl lg:text-2xl w-1/2 text-center mx-auto font-medium"
 
             const price = document.createElement("div");
             price.textContent = formatPrice(item.price);
 
             const container = document.createElement("div");
+            container.className = "border-dotted border-2 border-yellow-600 py-4 hover:bg-lime-600 item rounded-xl cursor-pointer"
             container.append(image, title, price);
             items.push(container);
         })
