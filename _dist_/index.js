3238438a0f81abc220d48d8870f05e6540154132
ionuser13 <ionuser13@users.noreply.github.com>
2022-11-11 15:58:18 +0000

    deploy: 4b156cf774c827e6e91e6cc8b2869ebca13fd9ec

--- _dist_/index.js
+++ _dist_/index.js
@@ -6,8 +6,17 @@
 const baseURL = "https://platzi-avo.vercel.app";
 const urlApi = "https://platzi-avo.vercel.app/api/avo";
 const myAppNode = document.querySelector("#app");
+myAppNode.className = "grid grid-cols-3 gap-4 max-w-5xl mx-auto";
 
-// Int API
+// Intl API 1.- dates formats 2.- currency formats
+
+const formatPrice = (price) => {
+    const newPrice = new window.Intl.NumberFormat("en-EN", {
+        style: "currency",
+        currency: "USD",
+    }).format(price)
+    return newPrice
+}
 
 async function fetchData() {
     const response = await fetch(urlApi);
@@ -16,12 +25,16 @@ async function fetchData() {
     try {
         dataRes.data.forEach((item) => {
             const image = document.createElement("img");
-            image.src = `${baseURL}${item.image}`
+            image.src = `${baseURL}${item.image}`;
+            image.className = "w-full cursor-pointer"
+
             const title = document.createElement("h2");
             title.textContent = item.name;
-            title.className = "text-2xl text-red-600"
+            title.className = "text-xl text-red-600 cursor-pointer"
+
             const price = document.createElement("div");
-            price.textContent = item.price;
+            price.textContent = formatPrice(item.price);
+
             const container = document.createElement("div");
             container.append(image, title, price);
             items.push(container);
