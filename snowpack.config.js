ab0ac98575eeeaa46a9d656af7cf38b36a9fcd1f
userjohn <johnchacpi479@gmail.com>
2022-11-14 14:17:18 -0500

    n attempt to deploy

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
     src: '/_dist_',
   },
   buildOptions: {
-    baseUrl: '"https://ionuser13.github.io/workshop-1"',
+    baseUrl: "https://ionuser13.github.io/workshop-1",
   },
 }
